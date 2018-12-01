970770008c3a1debfc861cceeef69f1e9af3c8d5
brob <brob@iastate.edu>
2018-12-01 15:15:17 -0600

    add about me

---
+++ blakearoberts/models/about.py
@@ -0,0 +1,31 @@
+from flask import url_for
+import yaml
+
+
+class About(object):
+    """
+    Defines a personal or school project.
+
+    Properties:
+        title: The title of the about me section.
+        description: The description of the project.
+        repo: The address of the repository.
+    """
+
+    def __init__(self, title='', image='', description='', contact=[]):
+        self.title = title
+        self.image = url_for('static', filename=image)
+        self.description = description
+        self.contact = contact
+
+    def load(yaml_file):
+        about = None
+        with open(yaml_file, 'r') as file:
+            raw_about = yaml.load(file)
+            about = About(
+                title=raw_about['title'],
+                image=raw_about['image'],
+                description=raw_about['description'],
+                contact=raw_about['contact'],
+            )
+        return about
--- blakearoberts/routes.py
+++ blakearoberts/routes.py
@@ -3,10 +3,13 @@ import os
 
 from blakearoberts import app
 from blakearoberts.models.project import Project
+from blakearoberts.models.about import About
 
 
 @app.route('/')
 def main():
     yaml_file = os.path.join(app.root_path, 'documents/projects.yaml')
     projects = Project.load_all(yaml_file)
-    return render_template('index.jinja', projects=projects)
+    about_file = os.path.join(app.root_path, 'documents/about.yaml')
+    about = About.load(about_file)
+    return render_template('index.jinja', about=about, projects=projects)
